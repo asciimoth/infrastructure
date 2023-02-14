5b92df89376740872bd92ea59ef26dcd32fd0a35
John Doe <>
2023-02-15 01:29:10 +0400

    AAAAAAAAAAAA

--- names/default.nix
+++ names/default.nix
@@ -4,6 +4,16 @@
   lib,
   inputs,
   ...
-}: {
-  environment.etc.naming.source = ../names;
+}: let
+  # perl -nle 'print if m{^[[:ascii:]]+$}' names/Minerals | awk '{$1=$1};1' | cut -d ' ' -f1 | cut -d '-' -f1 | uniq -i > names/Minerals.txt
+  nnormalise = pkgs.writeShellScriptBin "nnormalise" ''
+    tree $1 -fxainF -L 3 --prune --noreport | grep -v '/$' | grep -v '>' | tr -d '*'
+  '';
+in {
+  environment.etc.naming.source = ./names;
+  environment.systemPackages = with pkgs; [
+    tree
+    
+    nnormalise
+  ];
 }
