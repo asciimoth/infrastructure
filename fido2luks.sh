e7020105c0ad8528af449cd7cc0d4de6f2842855
Ascii Moth <ascii@moth.contact>
2023-07-20 09:18:24 -0700

    Update fido2luks.sh

--- fido2luks.sh
+++ fido2luks.sh
@@ -13,4 +13,4 @@ export FIDO2_LABEL="$PARTITION @ $NEWHOSTNAME"
 export CRED=$(echo "$DEVICEPIN" | fido2luks credential "$FIDO2_LABEL" --pin --pin-source /dev/stdin)
 echo $CRED >> $CREDFILE
 echo $'\n' >> $CREDFILE
-expect -c 'spawn fido2luks -i add-key $::env(PARTITION) $::env(CRED) --pin; expect "Authenticator PIN:"; send "$::env(DEVICEPIN)\r"; expect "Current password:"; send "$::env(PASSWORD)\r"; expect "New password:"; send "$::env(NEWPASS)\r"; interact'
+expect -c 'spawn fido2luks -i add-key $::env(PARTITION) $::env(CRED) --pin; expect "Authenticator PIN:"; send "$::env(DEVICEPIN)\r"; expect "Current password:"; send "$::env(PASSWORD)\r"; expect "Password to be added:"; send "$::env(NEWPASS)\r"; expect "Password to be added (again):"; send "$::env(NEWPASS)\r"; interact'
