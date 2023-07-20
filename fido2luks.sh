7b33106d0f86513605f1eff6b9603a5a45e2cfd5
Ascii Moth <ascii@moth.contact>
2023-07-20 08:22:48 -0700

    Update fido2luks.sh

--- fido2luks.sh
+++ fido2luks.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-# fido2luks.sh /dev/sda2 pcLexell ~/creds $PASSWORD
+# fido2luks.sh /dev/sda2 pcLexell ~/creds $PASSWORD "123456"
 
 export PARTITION=$1
 export NEWHOSTNAME=$2
 export CREDFILE=$3
 export PASSWORD=$4
+export DEVICEPIN=$5
 
 export FIDO2_LABEL="$PARTITION @ $NEWHOSTNAME"
-export CRED=$(fido2luks credential "$FIDO2_LABEL")
+export CRED=$(echo "$DEVICEPIN" | fido2luks credential "$FIDO2_LABEL" --pin --pin-source /dev/stdin)
 echo $CRED >> $CREDFILE
 echo $'\n' >> $CREDFILE
 expect -c 'spawn fido2luks -i add-key $::env(PARTITION) $::env(CRED) ; expect "Current password:"; send "$::env(PASSWORD)\r"; interact'
