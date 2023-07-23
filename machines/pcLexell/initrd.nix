08bd0fc0bc1420dcd86799be12ef46999d43fce5
Ascii Moth <ascii@moth.contact>
2023-07-23 09:35:46 -0700

    Update initrd.nix

--- machines/pcLexell/initrd.nix
+++ machines/pcLexell/initrd.nix
@@ -13,45 +13,21 @@
   inputs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    tree
-    #b3sum
-    #openssl
-  ];
-
-  #services.getty.autologinUser = "root";
-
   boot.initrd = {
-    extraUtilsCommands = ''
-      copy_bin_and_libs ${pkgs.bash}/bin/bash
-      copy_bin_and_libs ${pkgs.tree}/bin/tree
-      copy_bin_and_libs ${pkgs.gnupg}/bin/gpg2
-    '';
-    kernelModules = [
-      "uas"
-      "usbcore"
-      "usb_storage"
-      "vfat"
-      "nls_cp437"
-      "nls_iso8859_1"
-    ];
-    availableKernelModules = [
-      # For better luks encryption performance
-      "aesni_intel"
-      "cryptd"
-    ];
-    # TODO Add hello message with my contacts
     postDeviceCommands = let
-      path_hash = "50e97458df1ecadddd93c088873054dae35bedf51530e98df80d9c2ad814520a";
-      content_hash = "f1ac885c1f27071f89ff728d627fa7859f86e6526d9fd1ebd164a48d60515db1";
+      encrypted_key_b64 = "mDMEZLBs3xYJKwYBBAHaRw8BAQdAvQ1PVijYCri1r7qn9RPlLTycvOr9fLQPq8Ph5pN+1H60J0FTQ0lJIE1vdGggKF4wdzBeKSA8YXNjaWlAbW90aC5jb250YWN0PoiQBBMWCAA4FiEExchGWMz9fo5x3ukzrzrlT8OjXJ8FAmSwbN8CGwEFCwkIBwIGFQoJCAsCBBYCAwECHgECF4AACgkQrzrlT8OjXJ85bAEAr6+pe3ExhiCaPj9YJMsl4QPwu3Ni6o9hMQBIpffOEusBAJfYv2nUxUoHV1IXZeHpAKRmrsuD7ORlNfjDsfcoapoAuDMEZLBtFhYJKwYBBAHaRw8BAQdAmuoq5NgrBYZn6qXqchQOYexzEWS/1wrc/48N0uC+8VOI7wQYFggAIBYhBMXIRljM/X6Ocd7pM6865U/Do1yfBQJksG0WAhsCAIEJEK865U/Do1yfdiAEGRYIAB0WIQSCs1B4bnpeBAue0YS2czOfKVWMNQUCZLBtFgAKCRC2czOfKVWMNdfCAP4zvjZ1IjIbexFftopgkyUNgkZRNgYvIJMpnEw1vt0CrQEAva4DkaGUkFAR3BNh+ZpSXJrmOPv7oAoYpW5fiwtwqgLPBwEAv6KwvYvh/V8M9MtksVNsSTfuTOB+evTlJePr8ZwI9RkBALc0MQBvGQ4sX2UQOlNEJs5fT57we1W0dFaVux4BS2YBuDgEZLBtNxIKKwYBBAGXVQEFAQEHQGHOJSoGpSRuGCna5ogFq7dLgZax8+TVf5S4ue9h/ysRAwEIB4h4BBgWCAAgFiEExchGWMz9fo5x3ukzrzrlT8OjXJ8FAmSwbTcCGwwACgkQrzrlT8OjXJ8x7AD+N8b1rEGw0qIZN+Nq8fmQo4mpOAwoGS4gDBVTxFUmhpkBANhd9sJ/akLVfj9qYFpQqVe2z5/eQQSrRLbQUTv2OM8FuDMEZLBtXhYJKwYBBAHaRw8BAQdApj4HKiGXBF5qmc+iWwb6JW4rwKPRV75Vfs67UIm9GUWIeAQYFggAIBYhBMXIRljM/X6Ocd7pM6865U/Do1yfBQJksG1eAhsgAAoJEK865U/Do1yf/AAA/1mKVrWxdWg+Is0S9/ByUvCLGcOHZj825fABtQrNt9c0AP4nLBcBFCQ+CTWc8jhBerQyReyfu+sIF9g5OfPN1AtlDw==";
+      gpg_key_b64 = "";
     in
       lib.mkBefore ''
-        echo "Initrd shell started"
-        bash
+        echo "${encrypted_key_b64}" | base64 -d > encrypted_key
+        echo "${gpg_key_b64}" | base64 -d > gpg_key
       '';
-    #luks.devices."crypted" = {
-    #  keyFile = "/decrypt/decrypted_key";
-    #  preLVM = lib.mkForce false;
-    #};
+    luks.devices."crypted" = {
+      preLVM = lib.mkForce false;
+      gpgCard = {
+        encryptedPass = "/encrypted_key";
+        publicKey = "/gpg_key";
+      };
+    };
   };
 }
