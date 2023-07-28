40843f174b974dbccfd766dd605611cdd5af94c4
AsciiMoth <ascii@moth.contact>
2023-07-28 12:18:24 +0400

    dnscrypt-proxy configuration

--- machines/pcLexell/network.nix
+++ machines/pcLexell/network.nix
@@ -52,14 +52,33 @@ in {
   services.dnscrypt-proxy2 = {
     enable = true;
     settings = {
+      listen_addresses = [ "127.0.0.1:53" "[::1]:53" ];
+      max_clients = 256;
+      ipv4_servers = true;
       ipv6_servers = true;
+      block_ipv6 = false;
+      dnscrypt_servers = true;
+      doh_servers = true;
+      odoh_servers = true;
+      require_dnssec = false;
+      require_nofilter = true;
+      require_nolog = false;
+      cache = true;
       cache_size = 8192;
       #cache_min_ttl = 2400;
       #cache_max_ttl = 86400;
       #cache_neg_min_ttl = 60;
       #cache_neg_max_ttl = 600;
-      require_dnssec = true;
-      require_nofilter = true;
+      force_tcp = true;
+      timeout = 5000;
+      keepalive = 60; # DoT/DoH/ODoH
+      dnscrypt_ephemeral_keys = true;
+      tls_disable_session_tickets = true;
+      bootstrap_resolvers = [
+        "8.8.8.8:53"
+        "9.9.9.9:53"
+        "1.1.1.1:53"
+      ];
       sources.public-resolvers = {
         urls = [
           "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
@@ -67,13 +86,21 @@ in {
           "https://download.dnscrypt.info/dnscrypt-resolvers/v3/opennic.md"
           #"https://download.dnscrypt.info/dnscrypt-resolvers/v3/onion-services.md"
         ];
-        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        cache_file = "/var/lib/dnscrypt-proxy/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
       };
       server_names = [
-        "google"
-        "cloudflare"
+        #"google"
+        #"cloudflare"
+        "doh-ibksturm"
+        "publicarray-au2-doh"
+        "adguard-dns-doh"
+        "ahadns-doh-nl"
+        "ahadns-doh-la"
+        "ams-ads-doh-nl"
+        "adguard-dns-doh"
       ];
+      disabled_server_names = [ "yandex" ];
       forwarding_rules = "/etc/forwarding-rules";
     };
   };
@@ -94,7 +121,7 @@ in {
     description = "Resolve meshname and meship domains";
     serviceConfig = {
       Type = "simple";
-      User = "root";
+      User = "nobody";
       ExecStart = "${meshnamed}/bin/meshnamed -listenaddr 127.0.0.1:5353";
     };
   };
